ee30a40c1760dbb31ddf24015696c785accce7ae
Surjith S M <surjithctly@gmail.com>
2022-11-02 22:03:42 +0530

    setting up navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), alpinejs()]
 });
