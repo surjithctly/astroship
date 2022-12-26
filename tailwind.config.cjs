140dd1969c7f906b725f67250cb374add62176b9
Surjith S M <surjithctly@gmail.com>
2022-12-26 15:18:10 +0530

    add variable font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
