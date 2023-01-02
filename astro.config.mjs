b5941f1a72250469641cb18c70b82fc385956e27
Surjith S M <surjithctly@gmail.com>
2023-01-02 19:40:29 +0530

    remove alpinejs in favor of astro-navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,13 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [
     tailwind(),
-    alpinejs(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
