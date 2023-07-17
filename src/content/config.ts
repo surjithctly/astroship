ef4ca6b795c083c5afed1d4cc3cee7189a8c2f0c
Aldorax <climaxating@gmail.com>
2023-07-17 22:25:06 +0100

    changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -31,9 +31,24 @@ const teamCollection = defineCollection({
   }),
 });
 
+const testimonialCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    name: z.string(),
+    title: z.string(),
+    avatar: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    publishDate: z.string().transform(str => new Date(str)),
+    testimony: z.string(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
   'team': teamCollection,
+  'testimony': testimonialCollection,
 };
---
+++ src/content/testimony/testimony-1.md
@@ -0,0 +1,11 @@
+---
+draft: false
+name: "Janette Lynch"
+title: "buisness name"
+avatar: {
+    src: "https://images.unsplash.com/photo-1580489944761-15a19d654956?&fit=crop&w=280",
+    alt: "Janette Lynch"
+}
+publishDate: "2022-11-07 15:39"
+testimony: "I joined the teamof felojey to build my agro finance buisness. I joined the teamof felojey to build my agro finance buisness."
+---
