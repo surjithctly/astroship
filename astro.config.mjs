5f361542ccd684a991ce3bbe1c7ce4d53b638ec4
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-08-02 11:14:32 +0200

    Upgrade to astro 2.9.7

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [
