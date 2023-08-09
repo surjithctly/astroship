2881524095efea0807ffb10452869f040108f6e6
Michael Chase <127261330+mcchase94@users.noreply.github.com>
2023-08-09 13:32:39 -0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Join Our Waiting List</h1>
+      <p>Be the first to know when we launch!</p>
+    </header>
+  );
+}
+
+export default Header;
