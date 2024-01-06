7a790add743c371e12bc3653806f985b72430de7
yantao006 <yantao006@gmail.com>
2024-01-06 15:53:33 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://codecanyon.work",
   integrations: [tailwind(), mdx(), sitemap()],
 });
